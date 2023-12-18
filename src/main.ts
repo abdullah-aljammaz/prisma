505f53e4e2b5edcf7dc3e27270d034a3cc05e9b9
abdullah-aljammaz <crm1999@outlook.sa>
2023-12-18 18:15:27 +0300

    t

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Blog,User } from "@prisma/client";
 import { connectDB, prisma } from "./config/db";
 import express, { Request, Response } from 'express';
 import {v4 as uuidv4} from 'uuid';
@@ -16,14 +16,23 @@ app.get('/api/get',async (req:Request, res:Response)=>{
   return res.json(users)
 })
 
-
-
-app.post('/api/post',async (req:Request, res:Response)=>{
+app.post('/api/register',async (req:Request, res:Response)=>{
   const newUser = req.body as User
   await prisma.user.create({
   data:newUser
   })
-  res.json("Create Done")
+  res.json("Register Done")
+})
+
+app.post('/api/login',async (req:Request, res:Response)=>{
+  const {username, password} = req.body as User
+  const users = await prisma.user.findFirst({where:{username:username,password:password}});
+  if (users==null){
+    return res.json("Username Or Password Is Not correct")
+  }
+  else{
+    return res.json("Login successfully")
+  }
 })
 
 app.put('/api/put/:id', async (req:Request, res:Response)=>{
@@ -35,6 +44,60 @@ app.put('/api/put/:id', async (req:Request, res:Response)=>{
   })
   return res.json("update Done")
   })
+
+
+
+// Blog
+
+app.post("/blog/createBlog",async(req:Request,res:Response)=>{
+  const NewBlog = req.body as Blog
+  await prisma.blog.create({
+    data:NewBlog
+  })
+  res.json("Blog Is Created")
+})
+
+
+app.get('/blog/getalluserwithblog/:id',async (req:Request, res:Response)=>{
+  const {id} = req.params 
+  const getallblog = await prisma.blog.findMany ({where:{user_id:id},
+  select:{
+    title:true,
+    user_id:true,
+    createdata:true,
+    user:{select:{
+      username:true,
+      email:true}}
+  }})
+  return res.json(getallblog)
+})
+
+
+app.get('/blog/getAllBlog/',async (req:Request, res:Response)=>{
+  const getAllBlog = await prisma.blog.findMany({
+    select:{
+      title:true,
+      user_id:true,
+      createdata:true,
+      id:true,
+      user:{select:{
+        username:true,
+        email:true}}
+    }})
+  return res.json(getAllBlog)
+})
+
+
+app.delete('/blog/delete/:id',async (req:Request, res:Response)=>{
+  const {id} = req.params
+  await prisma.blog.delete({
+    where:{id:id}
+  })
+  res.json("Blog Has Been Deleted")
+})
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server Listing ${PORT}`)
